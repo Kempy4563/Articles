2124389bfcc216b44d3bcfdbe5bb4bdcda93b48e
leekempson <leekempson@hotmail.com>
2025-01-24 17:19:59 +0100

    Added CRUD operations

--- articles/app/urls.py
+++ articles/app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from app.views import home
-
+from app.views import (
+    ArticleListView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+)
 
 urlpatterns = [
-    # Home page
-    path("", home, name="home"),
+    path("", ArticleListView.as_view(), name="home"),
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
+    path("<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete_article"),
 ]
--- articles/app/views.py
+++ articles/app/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from app.models import Article, UserProfile
+from django.urls import reverse_lazy
+from app.models import Article
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DeleteView
+)
 
 
-def home(request):
-    articles = Article.objects.all()
-    print(articles)
-    return render(request, 'app/home.html', {'articles': articles})
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
+
+
+class ArticleCreateView(CreateView):
+    template_name = "app/create_article.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "twitter_post"]
+    success_url = reverse_lazy("home")
+
+
+class ArticleUpdateView(UpdateView):
+    template_name = "app/update_article.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "twitter_post"]
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = "app/delete_article.html"
+    model = Article
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
